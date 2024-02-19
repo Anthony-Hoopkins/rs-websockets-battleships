d7ce85e1a3d270534d22b858f93a38a0e9f30a89
Anthony M <aantonij@gmail.com>
2024-02-19 16:07:13 +0300

    init: create simple WS server

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from './src/http_server/index.js';
+import { WebSocketServer } from 'ws';
+import { onConnect } from './src/ws_server/ws.server.js';
 
 const HTTP_PORT = 8181;
+export const WS_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+export const wsServer = new WebSocketServer({ port: WS_PORT });
+
+wsServer.on('connection', onConnect);
+
+console.log(`Start WS server on the ${WS_PORT} port!`);
+
