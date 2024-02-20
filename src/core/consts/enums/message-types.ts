79ffb11b955d5674a1bd48bf6baabf5c1d0c09d7
Anthony M <aantonij@gmail.com>
2024-02-20 14:53:35 +0300

    feat: add ts

---
+++ src/core/consts/enums/message-types.ts
@@ -0,0 +1,8 @@
+export enum MessageTypes {
+  Registration = 'reg',
+  UpdateWinners = 'update_winners',
+
+  CreateRoom = 'create_room',
+  UpdateRoom = 'update_room',
+  AddUserToRoom = 'add_user_to_room',
+}
